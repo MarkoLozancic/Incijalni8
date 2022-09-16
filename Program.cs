4e4d4af76a99154d0589b3bb52a158326f865f29
User <User@LOZA>
2022-09-16 12:15:42 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Incijalni8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int prvi,drugi,treci,x, i;
+            prvi = 0;
+            drugi = 1;
+            Console.WriteLine("Unesi broj clana fibonnacijevog niza kojeg zelis da ti ispisem: ");
+            x = Convert.ToInt32(Console.ReadLine());
+            if (x > 2)
+            {
+                for (i = 3; i <= x; i++)
+                {
+                     treci=prvi + drugi ;
+                    prvi = drugi;
+                    drugi = treci;
+                    if (i == x)
+                    {
+                        Console.WriteLine("Vrijednost clana fibonnacijevog niza kojeg si odabrao je: " + treci);
+                        Console.ReadKey();
+                    }
+                }
+            }
+            else
+            { Console.WriteLine("Broj clana niza mora biti veci od 2!"); }
+
+
+        }
+    }
+}
